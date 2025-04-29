2485d3b4762ba64253c242ec848a78dfae2bac40
Caslu-Parra <lucasparra2@outlook.com>
2025-04-28 22:01:50 -0300

    feat: implementa CRUD aplicando estrutura de dados FILA

--- main.c
+++ main.c
@@ -8,7 +8,10 @@ typedef struct Paciente
     struct Paciente *prox;
 } Paciente;
 
-float calcularSeveridade()
+Paciente *inicio = NULL;
+Paciente *fim = NULL;
+
+float diagnosticarSeveridade()
 {
     float severidade = 0;
     int resposta;
@@ -50,25 +53,50 @@ Paciente *receberPaciente()
     }
     printf("Digite o nome do paciente: ");
     scanf("%s", paciente->nome);
-    paciente->severidade = calcularSeveridade();
+    paciente->severidade = diagnosticarSeveridade();
     paciente->prox = NULL;
     return paciente;
+}
+
+void ordenarFila(Paciente *paciente)
+{
+    Paciente *atual = inicio;
+    Paciente *anterior = NULL;
+
+    while (atual != NULL && atual->severidade > paciente->severidade)
+    {
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    if (anterior == NULL)
+    {
+        paciente->prox = inicio;
+        inicio = paciente;
+    }
+    else
+    {
+        anterior->prox = paciente;
+        paciente->prox = atual;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     int opcao = -1;
+   
     printf("## Bem-vindo ao sistema de triagem de pacientes! ##\n");
     while (opcao != 0)
     {
-        printf("\n------Menu------\n");
+        printf("\n------------Menu------------\n");
+        printf(" 0 - Encerrar atendimento\n");
         printf(" 1 - Receber paciente \n");
         printf(" 2 - Atender paciente \n");
         printf(" 3 - Imprimir fila de pacientes \n");
-        printf(" 0 - Encerrar atendimento\n");
         printf(" Digite a opcao desejada:  ");
         scanf("%d", &opcao);
 
+        system("cls || clear");
         switch (opcao)
         {
         case 1:
@@ -77,19 +105,41 @@ int main(int argc, char const *argv[])
             Paciente *novoPaciente = receberPaciente();
             if (novoPaciente != NULL)
             {
-                printf("Paciente %s recebido com severidade %.2f.\n", novoPaciente->nome, novoPaciente->severidade);
-                // Adicione o paciente à fila (implemente a lógica da fila)
+                ordenarFila(novoPaciente);
+                printf("Paciente %s foi recebido e diagnosticado com severidade %.2f.\n", novoPaciente->nome, novoPaciente->severidade);
             }
             break;
 
         case 2:
             printf("Opcao 2 selecionada: Atender paciente.\n");
-            // Implemente a lógica para atender o próximo paciente da fila
+            if (inicio != NULL)
+            {
+                Paciente *aux = inicio;
+                inicio = inicio->prox;
+                printf("Paciente %s atendido com sucesso!", aux->nome);
+                free(aux);
+                if (inicio == NULL)
+                    fim = NULL;
+            }
             break;
 
         case 3:
             printf("Opcao 3 selecionada: Imprimir fila de pacientes.\n");
-            // Implemente a lógica para imprimir a fila de pacientes
+            Paciente *atual = inicio;
+            int posicao = 1;
+            if (inicio == NULL)
+                printf("A fila de pacientes esta vazia.\n");
+            else
+            {
+                printf("\n--------------Fila de Pacientes---------------\n\n");
+                while (atual != NULL)
+                {
+                    printf("| #%d Posicao - Paciente %s (sev. %.2f)\n", posicao, atual->nome, atual->severidade);
+                    atual = atual->prox;
+                    posicao++;
+                }
+                printf("\n----------------------------------------------\n");
+            }
             break;
 
         case 0:
